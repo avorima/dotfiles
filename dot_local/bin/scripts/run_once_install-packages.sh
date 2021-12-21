250c80de2c73b7a983e5489195acd1fe00b7d3c6
Mario Valderrama <mario.valderrama@ionos.com>
2021-12-21 21:56:50 +0100

    Fix post-install script

--- dot_local/bin/scripts/run_once_install-packages.sh
+++ dot_local/bin/scripts/run_once_install-packages.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 case "$(grep '^ID=' /etc/os-release | cut -d= -f2)" in
     arch)
         echo "running post-install steps for Arch Linux"
-        sudo pacman -S --needed git base-devel go kubectl kubeseal \
+        sudo pacman -S --needed git man gopass base-devel go kubectl kubeseal \
             zsh-completions terraform alacritty direnv helm flameshot \
             minio-client neofetch neovim podman podman-docker networkmanager \
             networkmanager-openvpn skopeo skaffold jq xh go-yq ripgrep
@@ -12,7 +12,7 @@ case "$(grep '^ID=' /etc/os-release | cut -d= -f2)" in
             git clone https://aur.archlinux.org/yay.git /tmp/yay
             (
                 cd /tmp/yay
-                sudo makepkg -si
+                makepkg -si
             )
         fi
         yay -S --needed tmux-fastcopy nerd-fonts-complete kubectl-neat \
