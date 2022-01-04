7c9ee2a72312d2d3e75b334750f93f354471770e
Mario Valderrama <mario.valderrama@ionos.com>
2022-01-04 19:47:30 +0100

    Rewrite post-install script and remove vim-plug
    Less packages and install vim-plug using curl instead of having it in
    the repo.

---
+++ dot_local/bin/scripts/run_once_arch-postinstall.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+echo "running post-install steps for Arch Linux"
+
+if [[ ! -e ~/.config/nvim/autoload/plug.vim ]]; then
+    echo "installing vim-plug"
+    curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+echo "installing packages"
+sudo pacman -S --needed \
+    base-devel \
+    man \
+    vim \
+    neovim \
+    direnv \
+    alacritty \
+    tmux \
+    zsh \
+    zsh-completions \
+    go \
+    jq \
+    go-yq \
+    ripgrep \
+    gopass \
+    sops \
+    xh \
+    gopls \
+    python-lsp-server \
+    neofetch \
+    flameshot
+
+if ! command -v yay >/dev/null; then
+    echo "installing AUR helper"
+    git clone https://aur.archlinux.org/yay.git /tmp/yay
+    (
+        cd /tmp/yay
+        makepkg -si
+    )
+fi
+
+yay -S --needed tmux-fastcopy zsh-vi-mode nerd-fonts-source-code-pro
+printf "install additional packages? (y/N) "
+read -r yn
+if [[ $yn = "y" ]]; then
+    echo "installing additional development packages"
+    sudo pacman -S --needed \
+        kubectl \
+        kubeseal \
+        skopeo \
+        helm \
+        minio-client \
+        podman \
+        podman-docker
+    yay -S --needed kubectl-neat
+fi
+
+echo "finished post-install steps for Arch Linux"
