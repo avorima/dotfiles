f8b4704c8b2a727942b13457c4838e9895bcb852
Mario Valderrama <woldy401@gmail.com>
2025-04-17 09:51:10 +0200

    nvim: add telescope keybind for buffers

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,6 +4,18 @@ return {
         dependencies = {
             "nvim-lua/plenary.nvim"
         },
+        keys = {
+            { '<leader>ff', function() require("telescope.builtin").find_files() end, desc = '[F]ind [F]iles' },
+            { '<leader>fb', function() require("telescope.builtin").buffers() end, desc = '[F]ind [B]uffers' },
+            { '<leader>fg', function() require("telescope.builtin").live_grep() end, desc = '[F]ind using [G]rep' },
+            { '<leader>fw', function() require("telescope.builtin").grep_string() end , desc = '[F]ind current [W]ord' },
+            { '<leader>fh', function() require("telescope.builtin").help_tags() end , desc = '[F]ind [H]elp' },
+
+            { '<leader>fr', function() require("telescope.builtin").lsp_references() end, desc = 'LSP: [F]ind [R]eferences' },
+            { '<leader>fd', function() require("telescope.builtin").lsp_definitions() end, desc = 'LSP: [F]ind [D]efinitions' },
+            { '<leader>ft', function() require("telescope.builtin").lsp_type_definition() end, desc = 'LSP: [F]ind [T]ype definitions' },
+            { '<leader>fi', function() require("telescope.builtin").lsp_implementations() end, desc = 'LSP: [F]ind [I]mplementations' },
+        },
         config = function()
             require("telescope").setup({
                 defaults = {
@@ -42,18 +54,6 @@ return {
                     },
                 },
             })
-
-            local builtin = require("telescope.builtin")
-
-            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
-            vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind using [G]rep' })
-            vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[F]ind current [W]ord' })
-            vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[F]ind [H]elp' })
-
-            vim.keymap.set('n', '<leader>fr', builtin.lsp_references, { desc = 'LSP: [F]ind [R]eferences' })
-            vim.keymap.set('n', '<leader>fd', builtin.lsp_definitions, { desc = 'LSP: [F]ind [D]efinitions' })
-            vim.keymap.set('n', '<leader>ft', builtin.lsp_type_definitions, { desc = 'LSP: [F]ind [T]ype definitions' })
-            vim.keymap.set('n', '<leader>fi', builtin.lsp_implementations, { desc = 'LSP: [F]ind [I]mplementations' })
         end,
     },
 }
