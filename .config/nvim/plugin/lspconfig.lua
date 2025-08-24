ac6a4da84babcda7e66f2b6eb07f2458c59edbeb
Mario Valderrama <valderra.ma@outlook.com>
2025-08-24 10:21:04 +0200

    nvim/plugin: Update lua-ls settings

--- .config/nvim/plugin/lspconfig.lua
+++ .config/nvim/plugin/lspconfig.lua
@@ -149,6 +149,7 @@ lspconfig.lua_ls.setup {
       workspace = {
         -- Make the server aware of Neovim runtime files
         library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
       },
       -- Do not send telemetry data containing a randomized but unique identifier
       telemetry = {
@@ -156,4 +157,5 @@ lspconfig.lua_ls.setup {
       },
     },
   },
+  single_file_support = true,
 }
