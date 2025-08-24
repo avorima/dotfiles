c27bb416e8b7ceb3305b8588dd75ee33b0edbae0
Mario Valderrama <valderra.ma@outlook.com>
2025-08-24 10:19:47 +0200

    nvim/lspconfig: Add lua

--- dot_config/nvim/plugin/lspconfig.lua
+++ dot_config/nvim/plugin/lspconfig.lua
@@ -39,24 +39,24 @@ local on_attach = function(_, bufnr)
 
 end
 
+local lsp_flags = {
+      debounce_text_changes = 150,
+}
+
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 local servers = { 'pylsp', 'vimls', 'bashls', 'rust_analyzer' }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
-    flags = {
-      debounce_text_changes = 150,
-    }
+    flags = lsp_flags,
   }
 end
 
 lspconfig.gopls.setup {
   cmd = {"gopls", "serve"},
   on_attach = on_attach,
-  flags = {
-    debounce_text_changes = 150,
-  },
+  flags = lsp_flags,
   settings = {
     gopls = {
       buildFlags = { '-tags=integration,e2e' },
@@ -64,6 +64,37 @@ lspconfig.gopls.setup {
         -- fieldalignment = true,
         -- shadow = true,
         unusedparams = true,
+      },
+    },
+  },
+}
+
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+
+lspconfig.sumneko_lua.setup {
+  on_attach = on_attach,
+  flags = lsp_flags,
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = runtime_path,
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
       },
     },
   },
