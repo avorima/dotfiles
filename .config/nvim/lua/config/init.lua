2a682fc14dc95ba92a8cc853865dd70d8c94b1dd
Mario Valderrama <valderra.ma@outlook.com>
2025-08-24 10:21:04 +0200

    nvim: set helm filetype

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -2,3 +2,14 @@ require("config.settings")
 require("config.mappings")
 require("config.lazy")
 require("config.autocmd")
+
+vim.filetype.add({
+    extension = {
+        gotmpl = "gotmpl",
+    },
+    pattern = {
+        [".*/templates/.*%.tpl"] = "helm",
+        [".*/templates/.*%.ya?ml"] = "helm",
+        ["helmfile.*%.ya?ml"] = "helm",
+    },
+})
