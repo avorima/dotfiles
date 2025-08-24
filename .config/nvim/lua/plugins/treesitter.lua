24c06164e861720e80dc6b81f55ecae495dbcbe7
Mario Valderrama <valderra.ma@outlook.com>
2025-08-24 10:21:04 +0200

    nvim: fix treesitter-context

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,100 +1,98 @@
 return {
     {
         'nvim-treesitter/nvim-treesitter',
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-context",
+        },
         build = ":TSUpdate",
         cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-        keys = {
-            { "<c-space>", desc = "Increment Selection" },
-            { "<bs>", desc = "Decrement Selection", mode = "x" },
-        },
+        -- keys = {
+        --     { "<c-space>", desc = "Increment Selection" },
+        --     { "<bs>",      desc = "Decrement Selection", mode = "x" },
+        -- },
         config = function()
             local configs = require("nvim-treesitter.configs")
 
             configs.setup({
-              highlight = {
-                enable = true,
-                disable = {},
-                additional_vim_regex_highlighting = false,
-              },
-              indent = {
-                enable = false,
-                disable = {},
-              },
-              sync_install = false,
-              auto_install = true,
-              ensure_installed = {
-                "c",
-                "go",
-                "gomod",
-                "lua",
-                "perl",
-                "python",
-                "javascript",
-                "typescript",
-                "bash",
-                "vim",
-                "vimdoc",
-                "make",
-                "markdown",
-                "markdown_inline",
-                "json",
-                "yaml",
-                "helm",
-                "hcl",
-                "dockerfile",
-            },
+                highlight = {
+                    enable = true,
+                    disable = {},
+                    additional_vim_regex_highlighting = false,
+                },
+                indent = {
+                    enable = false,
+                    disable = {},
+                },
+                sync_install = false,
+                auto_install = true,
+                ensure_installed = {
+                    "c",
+                    "go",
+                    "gomod",
+                    "lua",
+                    "perl",
+                    "python",
+                    "javascript",
+                    "typescript",
+                    "bash",
+                    "vim",
+                    "vimdoc",
+                    "make",
+                    "markdown",
+                    "markdown_inline",
+                    "json",
+                    "yaml",
+                    "helm",
+                    "hcl",
+                    "dockerfile",
+                },
 
-            -- textobjects = {
-            --     select = {
-            --         enable = true,
-            --         lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-            --         keymaps = {
-            --             ["af"] = { query = "@function.outer", desc = "Select outer part of a function" },
-            --             ["if"] = { query = "@function.inner", desc = "Select inner part of a function" },
-            --             ["ac"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
-            --             ["ic"] = { query = "@conditional.inner", desc = "Select inner part of a conditional" },
-            --             ["ab"] = { query = "@block.outer", desc = "Select outer part of a block" },
-            --             ["ib"] = { query = "@block.inner", desc = "Select inner part of a block" },
-            --         },
-            --     },
-            --     swap = {
-            --         enable = true,
-            --         swap_next = {
-            --             ["<leader>a"] = "@parameter.inner",
-            --         },
-            --         swap_previous = {
-            --             ["<leader>A"] = "@parameter.inner",
-            --         },
-            --     },
-            --     move = {
-            --         enable = true,
-            --         set_jumps = true, -- whether to set jumps in the jumplist
-            --         goto_next_start = {
-            --             ["]]"] = "@function.outer",
-            --         },
-            --         goto_next_end = {
-            --             ["]["] = "@function.outer",
-            --         },
-            --         goto_previous_start = {
-            --             ["[["] = "@function.outer",
-            --         },
-            --         goto_previous_end = {
-            --             ["[]"] = "@function.outer",
-            --         },
-            --     },
-            -- },
-        })
-    end
+                -- textobjects = {
+                --     select = {
+                --         enable = true,
+                --         lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+                --         keymaps = {
+                --             ["af"] = { query = "@function.outer", desc = "Select outer part of a function" },
+                --             ["if"] = { query = "@function.inner", desc = "Select inner part of a function" },
+                --             ["ac"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
+                --             ["ic"] = { query = "@conditional.inner", desc = "Select inner part of a conditional" },
+                --             ["ab"] = { query = "@block.outer", desc = "Select outer part of a block" },
+                --             ["ib"] = { query = "@block.inner", desc = "Select inner part of a block" },
+                --         },
+                --     },
+                --     swap = {
+                --         enable = true,
+                --         swap_next = {
+                --             ["<leader>a"] = "@parameter.inner",
+                --         },
+                --         swap_previous = {
+                --             ["<leader>A"] = "@parameter.inner",
+                --         },
+                --     },
+                --     move = {
+                --         enable = true,
+                --         set_jumps = true, -- whether to set jumps in the jumplist
+                --         goto_next_start = {
+                --             ["]]"] = "@function.outer",
+                --         },
+                --         goto_next_end = {
+                --             ["]["] = "@function.outer",
+                --         },
+                --         goto_previous_start = {
+                --             ["[["] = "@function.outer",
+                --         },
+                --         goto_previous_end = {
+                --             ["[]"] = "@function.outer",
+                --         },
+                --     },
+                -- },
+            })
+        end
     },
 
     {
         "nvim-treesitter/nvim-treesitter-context",
-        cmd = { "TSContextEnable", "TSContextDisable", "TSContextToggle" },
-        config = function()
-            vim.keymap.set("n", "[c", function()
-                require("treesitter-context").go_to_context(vim.v.count1)
-            end, { silent = true })
-        end,
+        opts = {},
     },
 
     -- {
