08d6e9060f98f5cfd9b5e449239d083fe35db029
Mario Valderrama <valderra.ma@outlook.com>
2025-08-24 10:19:45 +0200

    [bin/update_go.sh] Fix update_go.sh

--- bin/update_go.sh
+++ bin/update_go.sh
@@ -5,8 +5,23 @@ VERSION=$1
 set -e
 
 cd ~/Downloads || exit 1
+
+echo "Fetching go $VERSION..."
 wget "https://dl.google.com/go/go$VERSION.linux-amd64.tar.gz"
-cp -r ~/.local/go ~/.local/go-backup
-tar -C ~/.local/go -xzf "go$VERSION.linux-amd64.tar.gz"
-go version
-rm -r ~/.local/go-backup
+
+echo "Copying old go files..."
+if command -v rsync >/dev/null; then
+    rsync -ah ~/.local/go ~/.local/go-backup
+else
+    cp -r  ~/.local/go ~/.local/go-backup
+fi
+
+echo "Deleting GOROOT"
+sudo rm -rf ~/.local/go
+
+echo "Extracting tarball..."
+tar -C ~/.local -xzf "go$VERSION.linux-amd64.tar.gz"
+
+echo -n "Remove backup? (y/N)"
+read -r yn
+[[ $yn == "y" ]] && sudo rm -rf ~/.local/go-backup
