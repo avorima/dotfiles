182d471c741eba44ddbd50aa11cd2b86195480c7
Mario Valderrama <valderra.ma@outlook.com>
2025-08-24 10:19:47 +0200

    nvim: Set shiftwidth=2 for lua and adjust files

--- .config/nvim/plugin/telescope.lua
+++ .config/nvim/plugin/telescope.lua
@@ -1,20 +1,20 @@
 require('telescope').setup{
-    pickers = {
-        find_files = {
-            theme = 'dropdown',
-            previewer = false,
-        },
-        lsp_references = {
-            theme = 'ivy',
-        },
-        lsp_definitions = {
-            theme = 'ivy',
-        },
-        lsp_type_definitions = {
-            theme = 'ivy',
-        },
-        lsp_implementations = {
-            theme = 'ivy',
-        },
+  pickers = {
+    find_files = {
+      theme = 'dropdown',
+      previewer = false,
     },
+    lsp_references = {
+      theme = 'ivy',
+    },
+    lsp_definitions = {
+      theme = 'ivy',
+    },
+    lsp_type_definitions = {
+      theme = 'ivy',
+    },
+    lsp_implementations = {
+      theme = 'ivy',
+    },
+  },
 }
